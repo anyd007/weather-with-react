f9ed5d1f57d2afe3a0dab10f8d6101b04bf55078
Andrzej Gębala <andrzej.gebala35@gmail.com>
2024-01-30 13:04:11 +0100

    upgrade hpa styles

---
+++ src/components/Time.js
@@ -0,0 +1,11 @@
+import '../styles/time.scss';
+
+const Time = () => {
+    return ( 
+        <div className="time">
+
+        </div>
+     );
+}
+ 
+export default Time;
--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -5,11 +5,13 @@ import Drops from './Drops';
 import HourDrops from './HourDrops';
 import '../styles/weather-details.scss';
 import Pressure from './Pressure';
+import Time from './Time';
 
 const WeatherDetails = ({ weather, fiveDay }) => {
  
 return (
     <div className="weather-details">
+      {weather && <Time weather={weather} />}
       {weather && <Temp weather={weather} fiveDay={fiveDay}/>}
       {fiveDay && <HourWeather fiveDay={fiveDay}/>}
       {weather && <Drops weather={weather}/>}
